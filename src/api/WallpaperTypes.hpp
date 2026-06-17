2cd422ba80b8f1db2b2e83e5a5ad2c09e3b471f4
Aromatic <symwww@outlook.com>
2026-06-17 15:32:37 +0800

    refactor(api): make wallpaper source types owned by public api

--- src/api/WallpaperTypes.hpp
+++ src/api/WallpaperTypes.hpp
@@ -5,4 +5,30 @@
 #include "runtime/input/InputEvent.hpp"
 #include "runtime/property/PropertyValue.hpp"
 #include "runtime/session/SessionState.hpp"
-#include "runtime/session/WallpaperTypes.hpp"
+
+#include <memory>
+#include <string>
+
+namespace wallpaper
+{
+class BackendFactory;
+
+enum class BackendType
+{
+    WEScene,
+    Web,
+    Image,
+    Video
+};
+
+struct WallpaperSource {
+    BackendType type;
+    std::string uri;
+    PropertyMap initialProperties;
+};
+
+struct SessionConfig {
+    std::shared_ptr<BackendFactory> backendFactory;
+    std::string                     cachePath;
+};
+} // namespace wallpaper
--- src/runtime/session/WallpaperTypes.hpp
+++ src/runtime/session/WallpaperTypes.hpp
@@ -1,30 +1,3 @@
 #pragma once
 
-#include "runtime/property/PropertyValue.hpp"
-
-#include <memory>
-#include <string>
-
-namespace wallpaper
-{
-class BackendFactory;
-
-enum class BackendType
-{
-    WEScene,
-    Web,
-    Image,
-    Video
-};
-
-struct WallpaperSource {
-    BackendType type;
-    std::string uri;
-    PropertyMap initialProperties;
-};
-
-struct SessionConfig {
-    std::shared_ptr<BackendFactory> backendFactory;
-    std::string                     cachePath;
-};
-} // namespace wallpaper
+#include "api/WallpaperTypes.hpp"
